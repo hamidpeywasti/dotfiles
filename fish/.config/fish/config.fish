0f6268ca28f201939b9dfdb8decb130cf1a177b0
Hamid Peywasti <hamid@respinar.com>
2025-01-07 22:36:45 +0330

    chnage lolcat configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ fish_config theme choose "Dracula Official"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    figlet -f slant "Fish Shell" | lolcat -S 1
+    figlet -f slant "Fish Shell" | lolcat -h 0.5
 end
 
 # fish_config theme choose "dracula"
