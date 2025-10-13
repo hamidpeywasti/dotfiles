0511bdbc51e6b08d21995e8b0e627a9dcb3e8fc7
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-10-13 21:51:10 +0330

    adding paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,21 @@
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/bin
+fish_add_path /opt/tx
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.deno/bin
+fish_add_path $HOME/.npm-global/bin
+fish_add_path $HOME/.zig
+fish_add_path $HOME/.opencode/bin
+
 fish_config theme choose "Dracula Official"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    figlet -f slant "Fish Shell" | lolcat -h 0.5
+    figlet -f slant "Fish Shell" | lolcat -S 1.0
 end
 
 # fish_config theme choose "dracula"
 starship init fish | source
 
-zoxide init fish | source
+# zoxide init fish | source
